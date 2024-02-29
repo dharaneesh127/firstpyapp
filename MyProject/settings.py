36494b621da3d0b8b28156bdd36e0adcb9e4d9f0
dharaneesh-cmd <dharaneesh127@gmail.com>
2024-03-01 01:48:37 +0530

    hope its the last commit, forth commit

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['my-py-app.azurewebsites.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    "whitenoise.runserver_nostatic",
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",   
 ]
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")  
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")  
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
